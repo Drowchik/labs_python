7911a7346dbf39c97e6e29cead790d31798693b0
natal <natal@CLOWN>
2023-09-14 23:31:54 +0400

    add_rating+title

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import requests
 
 URL = "https://www.livelib.ru/reviews/~2#reviews"
 html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"})
+html_page.encoding = 'utf-8'
 src=html_page.text
 # html_page.text - хранит html код веб-страницы
 
 soup = BeautifulSoup(src, "lxml")
-print(soup)
-title = soup.find("div", class_="lenta-card__mymark")
-print(title)
+
+rating = soup.find_all("span", {"class":"lenta-card__mymark"})
+title = soup.find_all("div", {"id":"lenta-card__text-review-escaped"})
+
+for key in rating:
+    print(float(key.text))
+    
+# for key in title:
+#     print(key.text)
+
      
