b1f8c985555c9adf92eb7c0e1e3eb7ec2e571c39
natal <natal@CLOWN>
2023-09-14 19:01:28 +0400

    getting html

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world")
+import os
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://www.livelib.ru/reviews/~2#reviews"
+html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"})
+src=html_page.text
+# html_page.text - хранит html код веб-страницы
+
+soup = BeautifulSoup(src, "lxml")
+print(soup)
+title = soup.find("div", class_="lenta-card__mymark")
+print(title)
+     
