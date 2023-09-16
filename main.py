af9671cfd1f9b4e7240c27484983e40d5ac43ee3
natal <natal@CLOWN>
2023-09-16 21:22:15 +0400

    parsing 4 and 5 stars

--- main.py
+++ main.py
@@ -1,33 +1,52 @@
 import os
+from time import sleep
 from bs4 import BeautifulSoup
 import requests
 
-number_list = 2
-URL = f"https://www.livelib.ru/reviews/~{number_list}#reviews" #ссылка номер отвечает за страницу
-html_page = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"})
-html_page.encoding = 'utf-8'
-src=html_page.text
-# html_page.text - хранит html код веб-страницы
-soup = BeautifulSoup(src, "lxml")
+headers ={
+    "Accept" : "*/*",
+    "User-Agent" : "Mozilla/5.0"
+}
 
-rating = soup.find_all("span", {"class":"lenta-card__mymark"})
-title = soup.find_all("a", "lenta-card__book-title")
-review = soup.find_all("div", {"id":"lenta-card__text-review-escaped"})
-
-for i in range(len(rating)):
-    if float(rating[i].text) == 5:
-        print(title[i].text.strip())
-    
-def make_dir():
+def make_dir() -> None:
     dirs = ['dataset/1', 'dataset/2', 'dataset/3', 'dataset/4', 'dataset/5']
     if not os.path.isdir('dataset'):
         os.mkdir('dataset')
     for dir_name in dirs:
         if not os.path.isdir(dir_name):
             os.mkdir(dir_name)
-#     print(key.text.strip())
-    
-# for key in review:
-#     print(key.text)
 
-     
+number_list=1
+make_dir()
+number_5 = 0
+number_4 = 0
+while number_5<1001 and number_4<1001:
+    sleep(4*number_list)
+    print(number_list)
+    URL = f"https://www.livelib.ru/reviews/search/rp;ow/~{number_list}#reviews" 
+    
+    # ссылка номер отвечает за страницу
+    html_page = requests.get(URL, headers)
+    html_page.encoding = 'utf-8'
+    src = html_page.text
+    number_list += 1
+    # html_page.text - хранит html код веб-страницы
+    soup = BeautifulSoup(src, "lxml")
+
+    rating = soup.find_all("span", {"class": "lenta-card__mymark"})
+    title = soup.find_all("a", "lenta-card__book-title")
+    review = soup.find_all("div", {"id": "lenta-card__text-review-escaped"})
+
+    for i in range(len(rating)-1):
+        if float(rating[i].text) == 5.0 and number_5 < 1000:
+            number_5_1 = str(number_5 + 1).zfill(4)
+            number_5 += 1
+            name_file = f'dataset/5/{number_5_1}.txt'
+            with open(name_file, 'w', encoding="utf-8") as f:
+                f.write(title[i].text.strip() + '\n' + review[i].text.strip())
+        if float(rating[i].text) == 4.0 and number_4 < 1000:
+            number_4_1 = str(number_4 + 1).zfill(4)
+            number_4 += 1
+            name_file = f'dataset/4/{number_4_1}.txt'
+            with open(name_file, 'w', encoding="utf-8") as f:
+                f.write(title[i].text.strip() + '\n' + review[i].text.strip())
